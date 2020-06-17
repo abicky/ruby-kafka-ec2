3538e4df98f7e71b11ef92067f64617df186795d
abicky <takeshi.arabiki@gmail.com>
2020-06-18 00:51:12 +0900

    Initialize gem by "bundle gem --no-coc ruby-kafka-ec2"

---
+++ lib/ruby/kafka/ec2.rb
@@ -0,0 +1,10 @@
+require "ruby/kafka/ec2/version"
+
+module Ruby
+  module Kafka
+    module Ec2
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/ruby/kafka/ec2/version.rb
@@ -0,0 +1,7 @@
+module Ruby
+  module Kafka
+    module Ec2
+      VERSION = "0.1.0"
+    end
+  end
+end
